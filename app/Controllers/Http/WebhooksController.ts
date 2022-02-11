70e046e8c1a113592d617f275fb965bdc3d20c67
amadeujunior1101 <amadeujunior00@gmail.com>
2022-02-10 22:59:20 -0300

    adicionado endpoint para webhook

---
+++ app/Controllers/Http/WebhooksController.ts
@@ -0,0 +1,18 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class WebhooksController {
+  public async iuguStatusChangeTrigger({ request, response }: HttpContextContract) {
+    try {
+      console.log(request.headers())
+      if (request.headers().authorization !== 'Basic IVFTNG1RJiokMk5pYUJIbiVNKnc6')
+        return response.status(401)
+
+      console.log('gatilho Iugu teste')
+      const result = request.all()
+      console.log('Iugu data', result)
+      return response.ok(result)
+    } catch (error) {
+      console.log('iugu webhook error', error)
+    }
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -19,6 +19,9 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+Route.group(() => {
+  Route.post('/', 'IuguWebHooksController.iuguStatusChangeTrigger')
+}).prefix('/iuguwebhook')
 
 Route.group(() => {
   Route.post('/users', 'UserController.store')
